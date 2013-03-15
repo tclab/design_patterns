36e8ededb955be2e1e6683bc42e7b3d401bc6d86
jtorocan <tclabcontact@gmail.com>
2013-03-15 12:48:36 -0500

    Initializing patterns implementation

---
+++ src/co/tclab/patterns/structural/decorator/Test.java
@@ -0,0 +1,11 @@
+package co.tclab.patterns.structural.decorator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: juan
+ * Date: 3/15/13
+ * Time: 12:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
